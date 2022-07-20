91eff4db1189dc3a7023dc8263a370bc4b3775a0
Mahmoud Mohammed Ali <70591959+MahmoudMuhammedAli@users.noreply.github.com>
2022-07-20 05:27:18 +0200

    Update browser.js

--- browser.js
+++ browser.js
@@ -3,9 +3,8 @@ const puppeteer = require("puppeteer");
 async function startBrowser() {
   let browser;
   try {
-    console.log("Opening the browser......");
     browser = await puppeteer.launch({
-      headless: false,
+      headless: true,
       args: [ "--disable-setuid-sandbox" ],
       ignoreHTTPSErrors: true,
     });
